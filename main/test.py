2ee91ae2ea3b38e869186d1f7232e517cfcb3a3f
Jenil060402 <jenil060402@gmail.com>
2024-03-09 11:31:32 +0530

    Reviewed some changes

--- main/test.py
+++ main/test.py
@@ -6,26 +6,31 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from pymongo import MongoClient
 import pymongo
-import sys
 
 class AddressBook(App):
     def build(self):
         uri = 'mongodb+srv://jenil060402:Je10514912nil%40@project1.agjazpp.mongodb.net/?retryWrites=true&w=majority&appName=Project1'
     
+        #trying to connect to the databse using idp
         try:
             self.client = pymongo.MongoClient(uri)
         except  pymongo.errors.ConfigurationError:
             print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
-            sys.exit(1)
-        self.db = self.client['address_book']  # Select database
-        self.collection = self.db['contacts']  # Select collection
+        try:
+            self.db = self.client['address_book']  # Selecting the databse
+        except pymongo.errors.ConnectionFailure:
+            print('No db found')
+        try:
+            self.collection = self.db['contacts']  # Selecting the collectn
+        except pymongo.errors.ConnectionInvalid:
+            print('No such collection found')
 
         # layout = BoxLayout(orientation='vertical')
         layout = GridLayout(cols=2, spacing=10, padding=10)
 
 
         self.name_input = TextInput(hint_text='Enter Name', multiline=False)
-        self.email_input = TextInput(hint_text='Enter Email', multiline=False)
+        self.email_input= TextInput(hint_text='Enter Email', multiline=False)
         self.phone_input = TextInput(hint_text='Enter Phone Number', multiline=False)
         self.address_input = TextInput(hint_text='Enter Address', multiline=False)
         add_button = Button(text='Add Contact')
@@ -34,7 +39,7 @@ class AddressBook(App):
         self.email_input.bind(text=self.check_and_update_address)
         self.phone_input.bind(text=self.check_and_update_address)
         
-        layout.add_widget(Label(text='Name:', size_hint=(None, None), size=(100, 50), halign='left', valign='middle')) 
+        layout.add_widget(Label(text='Name:')) 
         layout.add_widget(self.name_input)
         layout.add_widget(Label(text='Email:'))
         layout.add_widget(self.email_input)
